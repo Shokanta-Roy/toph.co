5f3a0db5b8d686a270503425bec3a1a37c9c9ff2
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-03-06 02:19:55 +0600

    Add files via upload

---
+++ Graph Theory/How Many Ways.cpp
@@ -0,0 +1,11 @@
+//shokanta_roy
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int i=1;i<=tc;i++)
+        cout<<"Case "<<i<<": 1\n";
+    return 0;
+}
